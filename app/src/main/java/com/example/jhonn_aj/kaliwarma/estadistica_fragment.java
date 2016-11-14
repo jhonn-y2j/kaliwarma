fa8195cd1dbda57d1f99ab775ddf2cea8a8e8448
Jhonn Ayala Chero <y2jz28@gmail.com>
2016-11-14 18:56:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jhonn_aj/kaliwarma/estadistica_fragment.java
@@ -0,0 +1,21 @@
+package com.example.jhonn_aj.kaliwarma;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jhonn_aj on 14/11/2016.
+ */
+public class estadistica_fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment_estadistica,container,false);
+        return view;
+    }
+}
